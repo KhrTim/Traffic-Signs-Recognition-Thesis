b33623e5e032b0d2c4603b00ae8ff22081f78edb
KhrTim <khairulov00@gmail.com>
2023-09-18 01:01:23 +0300

    Initial commit

---
+++ model_controller.py
@@ -0,0 +1,23 @@
+import torch
+
+
+class ModelController:
+    def __init__(self, model_path=None):
+        self.model = self.load_model(model_path)
+        self.model.max_det = 10
+        self.classes = self.model.names
+        # self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        # self.model.to(self.device) # may be mistake
+
+    def load_model(self, model_name):
+        assert model_name
+        if model_name:
+            model = torch.hub.load('ultralytics/yolov5',
+                                   'custom', path=model_name, device='cpu', force_reload=True)
+        return model
+
+    def get_model(self):
+        return self.model
+
+    def get_classes(self):
+        return self.classes
---
+++ source_mode.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+class SourceMode(Enum):
+    IMAGE = 1
+    VIDEO = 2
+    CAMERA = 3
